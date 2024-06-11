9f43dc7f280b3c1ff8417eb2571fbf0149751365
Reyom <Reyomalmalki@gmail.com>
2024-06-11 08:24:51 +0300

    HW3 new

---
+++ src/test/java/TestingClass.java
@@ -0,0 +1,28 @@
+import org.example.Person;
+import org.example.Student;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class TestingClass {
+    private Person p1;
+    private Student s1;
+
+
+    @BeforeAll
+    public static void init(){
+        System.out.println("Star before all");
+    }
+
+    @Test
+    public void testPerson() {
+        String name = "Ali";
+        int age = 23;
+        String address = "Riyadh";
+
+        String actual = "Name: " + name + ", Age: " + age + ", Address: " + address;
+        String expected = "Name: Ali, Age: 23, Address: Riyadh";
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
